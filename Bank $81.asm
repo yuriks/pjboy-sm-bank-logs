90d2242bb6eb3a4542d3402f316b873c506c40b8
PJBoy <gcupcakekid@gmail.com>
2024-06-15 14:52:55 +0000

    Update logs from website

--- Bank $81.asm
+++ Bank $81.asm
@@ -5972,8 +5972,8 @@ $81:B35A 9C E2 09    STZ $09E2  [$7E:09E2]  ; Disable Japanese text
 $81:B35D 9C E4 09    STZ $09E4  [$7E:09E4]  ; Disable moonwalk
 $81:B360 9C EA 09    STZ $09EA  [$7E:09EA]  ; Disable HUD auto-cancel
 $81:B363 A9 01 00    LDA #$0001             ;\
-$81:B366 8D E6 09    STA $09E6  [$7E:09E6]  ;} $09E6 = 1
-$81:B369 8D E8 09    STA $09E8  [$7E:09E8]  ; $09E8 = 1
+$81:B366 8D E6 09    STA $09E6  [$7E:09E6]  ;} Debug disable Samus placement mode flag = 1
+$81:B369 8D E8 09    STA $09E8  [$7E:09E8]  ; $09E8 = 1 (never read)
 $81:B36C A2 00 00    LDX #$0000             ;\
 $81:B36F A9 00 00    LDA #$0000             ;|
                                             ;|
